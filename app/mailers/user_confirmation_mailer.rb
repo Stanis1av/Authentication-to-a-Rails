d9b40515a62cdfa3bce674cd0bdc99d42020ae7e
Stanis1av <krasavin.stanislaus@gmail.com>
2021-11-19 01:12:08 +0300

    Adding Bootstrap to the project and other minor changes

--- app/mailers/user_confirmation_mailer.rb
+++ app/mailers/user_confirmation_mailer.rb
@@ -7,10 +7,10 @@ class UserConfirmationMailer < Devise::Mailer
 
   def confirmation_instructions(record, token, opts={})
 
-    logger.debug "#{'=' * 110 }"
-    logger.debug "token: #{token.inspect}"
-    logger.debug "#{'=' * 110 }"
-    logger.debug "record: #{record.inspect}"
+    # logger.debug "#{'=' * 110 }"
+    # logger.debug "token: #{token.inspect}"
+    # logger.debug "#{'=' * 110 }"
+    # logger.debug "record: #{record.inspect}"
 
     @user = record
     @token = token
@@ -19,19 +19,19 @@ class UserConfirmationMailer < Devise::Mailer
     @send_email = SibApiV3Sdk::TransactionalEmailsApi.new # api instance
     @mail_letter = SibApiV3Sdk::SendSmtpEmail.new # SendSmtpEmail | Values to send a transactional email
 
-    logger.debug "#{'=' * 110 }"
-    logger.debug "send_email: \n#{@send_email.inspect}"
-    logger.debug "#{'=' * 110 }"
-    logger.debug "mail_letter: \n#{@mail_letter.inspect}"
-    logger.debug "#{'=' * 110 }"
+    # logger.debug "#{'=' * 110 }"
+    # logger.debug "send_email: \n#{@send_email.inspect}"
+    # logger.debug "#{'=' * 110 }"
+    # logger.debug "mail_letter: \n#{@mail_letter.inspect}"
+    # logger.debug "#{'=' * 110 }"
 
     html = render_to_string template: 'users/mailer/confirmation_instructions'
     document = Roadie::Document.new html
     document = document.transform
 
-    logger.debug "#{'=' * 110 }"
-    logger.debug "HTML mail letter: \n#{document}"
-    logger.debug "#{'=' * 110 }"
+    # logger.debug "#{'=' * 110 }"
+    # logger.debug "HTML mail letter: \n#{document}"
+    # logger.debug "#{'=' * 110 }"
 
     @mail_letter.sender = {
       "name": "FindWorker",
@@ -46,16 +46,16 @@ class UserConfirmationMailer < Devise::Mailer
       "name": "FindWorker"
     }
 
-    logger.debug "Final version mail letter: \n#{@mail_letter}"
-    logger.debug "#{'=' * 110 }"
+    # logger.debug "Final version mail letter: \n#{@mail_letter}"
+    # logger.debug "#{'=' * 110 }"
 
     begin
       # Send a transactional email
       result = @send_email.send_transac_email(@mail_letter)
 
 
-      logger.debug "Result: \n#{result}"
-      logger.debug "#{'=' * 110 }"
+      # logger.debug "Result: \n#{result}"
+      # logger.debug "#{'=' * 110 }"
 
       return result
 
