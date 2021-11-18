77ebdcfce82629231dc2ba00a57edbada0f8f80b
Stanis1av <krasavin.stanislaus@gmail.com>
2021-11-18 23:57:48 +0300

    Overriding the class responsible for sending emails. And overriding devise_mailer method to model user.rb (optional)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  # def devise_mailer # optional
+  #   UserConfirmationMailer
+  # end
 end
